28d7c7101f79627a15c5897a8ad4e7241196f617
Max Mouratov <mmouratov@gmail.com>
2015-08-08 01:16:08 +0500

    new: Board.move

--- honeycaml/board.ml
+++ honeycaml/board.ml
@@ -6,6 +6,16 @@ type t =
     height: int;
   }
 
+type dir = L | R | LD | RD | LU | RU
+
+
+let reverse_dir = function
+  | L -> R
+  | R -> L
+  | LD -> RU
+  | RD -> LU
+  | LU -> RD
+  | RU -> LD
 
 let make ~width ~height =
   let arr = Array.make (width * height) false in
@@ -20,8 +30,26 @@ let get (b: t) (c: Cell_t.t) =
 let set (b: t) (c: Cell_t.t) (v: bool) =
   if c.x < b.width && c.y < b.height then
     b.arr.(c.y * b.width + c.x) <- v
+
+let rec move (b: t) (c: Cell_t.t) dir ~len : Cell_t.t =
+  if len < 0 then
+    move b c (reverse_dir dir) (-len)
+  else if (c.y land 1) = 0 then
+    match dir with
+      | L  -> { c with x = c.x - len }
+      | R  -> { c with x = c.x + len }
+      | LD -> { x = c.x - ((len+1) / 2); y = c.y + len }
+      | RD -> { x = c.x - (len / 2); y = c.y + len }
+      | LU -> { x = c.x - ((len+1) / 2); y = c.y - len }
+      | RU -> { x = c.x - (len / 2); y = c.y - len }
   else
-    ()
+    match dir with
+      | L  -> { c with x = c.x - len }
+      | R  -> { c with x = c.x + len }
+      | LD -> { x = c.x - (len / 2); y = c.y + len }
+      | RD -> { x = c.x - ((len+1) / 2); y = c.y + len }
+      | LU -> { x = c.x - (len / 2); y = c.y - len }
+      | RU -> { x = c.x - ((len+1) / 2); y = c.y - len }
 
 let copy (b: t) =
   { b with arr = Array.copy b.arr }
--- honeycaml/board.mli
+++ honeycaml/board.mli
@@ -1,6 +1,8 @@
 
 type t
 
+type dir = L | R | LD | RD | LU | RU
+
 
 val make: width: int -> height: int -> t
 
@@ -9,3 +11,5 @@ val set: t -> Cell_t.t -> bool -> unit
 val get: t -> Cell_t.t -> bool option
 
 val copy: t -> t
+
+val move: t -> Cell_t.t -> dir -> len: int -> Cell_t.t
