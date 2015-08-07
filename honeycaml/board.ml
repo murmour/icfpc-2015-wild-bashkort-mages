54afb4353491107c31fe2267a4a8e4f694546914
Max Mouratov <mmouratov@gmail.com>
2015-08-08 02:01:42 +0500

    new: Board.rotate

--- honeycaml/board.ml
+++ honeycaml/board.ml
@@ -8,6 +8,8 @@ type t =
 
 type dir = L | R | LD | RD | LU | RU
 
+type rot_dir = CW | CCW
+
 
 let reverse_dir = function
   | L -> R
@@ -31,25 +33,35 @@ let set (b: t) (c: Cell_t.t) (v: bool) =
   if c.x < b.width && c.y < b.height then
     b.arr.(c.y * b.width + c.x) <- v
 
-let rec move (b: t) (c: Cell_t.t) dir ~len : Cell_t.t =
+let rec move (c: Cell_t.t) dir ~len : Cell_t.t =
   if len < 0 then
-    move b c (reverse_dir dir) (-len)
+    move c (reverse_dir dir) (-len)
   else if (c.y land 1) = 0 then
     match dir with
-      | L  -> { c with x = c.x - len }
-      | R  -> { c with x = c.x + len }
+      | L -> { c with x = c.x - len }
+      | R -> { c with x = c.x + len }
       | LD -> { x = c.x - ((len+1) / 2); y = c.y + len }
       | RD -> { x = c.x - (len / 2); y = c.y + len }
       | LU -> { x = c.x - ((len+1) / 2); y = c.y - len }
       | RU -> { x = c.x - (len / 2); y = c.y - len }
   else
     match dir with
-      | L  -> { c with x = c.x - len }
-      | R  -> { c with x = c.x + len }
+      | L -> { c with x = c.x - len }
+      | R -> { c with x = c.x + len }
       | LD -> { x = c.x - (len / 2); y = c.y + len }
       | RD -> { x = c.x - ((len+1) / 2); y = c.y + len }
       | LU -> { x = c.x - (len / 2); y = c.y - len }
       | RU -> { x = c.x - ((len+1) / 2); y = c.y - len }
+
+let rotate ~(pivot: Cell_t.t) (p: Cell_t.t) dir : Cell_t.t =
+  let ydiff = pivot.y - p.y in
+  let temp = move pivot LU ~len:ydiff in
+  let xdiff = temp.x - p.x in
+  match dir with
+    | CW ->
+        move (move pivot RU ~len:ydiff) LU ~len:xdiff
+    | CCW ->
+        move (move pivot L ~len:ydiff) LD ~len:xdiff
 
 let copy (b: t) =
   { b with arr = Array.copy b.arr }
--- honeycaml/board.mli
+++ honeycaml/board.mli
@@ -3,6 +3,8 @@ type t
 
 type dir = L | R | LD | RD | LU | RU
 
+type rot_dir = CW | CCW
+
 
 val make: width: int -> height: int -> t
 
@@ -12,4 +14,6 @@ val get: t -> Cell_t.t -> bool option
 
 val copy: t -> t
 
-val move: t -> Cell_t.t -> dir -> len: int -> Cell_t.t
+val move: Cell_t.t -> dir -> len: int -> Cell_t.t
+
+val rotate: pivot: Cell_t.t -> Cell_t.t -> rot_dir -> Cell_t.t
