c3a85c6fc19efc38cbacc00828a19e7e1144db5b
Max Mouratov <mmouratov@gmail.com>
2015-08-08 23:08:19 +0500

    :-(

--- honeycaml/honey.ml
+++ honeycaml/honey.ml
@@ -25,7 +25,7 @@ let _ =
        " <int> Number of processor cores available");
 
       ("-p", Arg.String (fun s -> power_phrases := s :: !power_phrases),
-       " <string> Phrase of power.");
+       " <string> Phrase of power");
     ])
     (fun _ -> ())
     ("Usage: " ^ Sys.argv.(0) ^ " [options]")
@@ -35,7 +35,7 @@ let games: Js_Game_t.t list =
   !input_files |> List.rev |> List.map (fun path ->
     Js_Game_j.t_of_string (File.with_file_in path IO.read_all))
 
-let solutions: Js_Solution_t.t =
+let solutions: Js_Solution_t.problem list =
   RefList.collecting (fun list ->
     games |> List.iter (fun (g: Js_Game_t.t) ->
       g.sourceSeeds |> List.iter (fun seed ->
