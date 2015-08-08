f660222e899f5c9db17ad4a15abcd3e086a1a5ae
purplepie <linesprower@gmail.com>
2015-08-08 22:46:57 +0500

    powerwords support in visualizer

--- python-ws/davar/scorer.py
+++ python-ws/davar/scorer.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
     if len(sys.argv) != 3:
         print('Usage: scorer.py solver version')
         sys.exit(1)
-    icfp_api.send_all_solutions(sys.argv[1], int(sys.argv[2]))
+    icfp_api.score_all_solutions(sys.argv[1], int(sys.argv[2]))
