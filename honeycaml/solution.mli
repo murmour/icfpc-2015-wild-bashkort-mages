34bb26f4b7d7e6e5e667f17bf64829bedf339482
Max Mouratov <mmouratov@gmail.com>
2015-08-08 22:07:03 +0500

    :-(

--- honeycaml/solution.mli
+++ honeycaml/solution.mli
@@ -1,5 +1,17 @@
 
 type t = char list
 
+type command =
+  | MoveE
+  | MoveW
+  | MoveSE
+  | MoveSW
+  | TurnCW
+  | TurnCCW
+
 
 val to_string: t -> string
+
+val decode_command: char -> command
+
+val encode_command: command -> char
