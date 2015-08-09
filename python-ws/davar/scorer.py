dfafd963df0c8da709538fa3eb83be06197b7929
Max Mouratov <mmouratov@gmail.com>
2015-08-09 16:43:35 +0500

    fix

--- python-ws/davar/scorer.py
+++ python-ws/davar/scorer.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         icfp_api.score_and_mark_all_solutions(None, None)
     elif len(sys.argv) == 3:
-        icfp_api.score_and_mark_all_solutions(unpack_some(sys.argv[1]), int(sys.argv[2]))
+        icfp_api.score_and_mark_all_solutions(sys.argv[1], int(sys.argv[2]))
     elif len(sys.argv) == 4:
         if int(sys.argv[3]) > 0:
             icfp_api.score_and_mark_all_solutions(sys.argv[1], int(sys.argv[2]))
